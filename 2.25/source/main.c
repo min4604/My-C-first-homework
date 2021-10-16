cdd0fa65083bb74a4624e170dfafbda19bd9df51
min4604 <mina4604sh@gmail.com>
2021-10-16 17:28:48 +0800

    2.25finish

---
+++ 2.25/source/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char text[] = "\
+ PPPPPPPPP\n\
+     P   P\n\
+     P   P\n\
+     P   P\n\
+      P P \n\
+          \n\
+   JJ     \n\
+  J       \n\
+ J        \n\
+  J       \n\
+   JJJJJJJ\n\
+          \n\
+ DDDDDDDDD\n\
+ D       D\n\
+ D       D\n\
+  D     D \n\
+   DDDDD  \n";
+
+void main(void)
+{
+	printf(text);
+	system("Pause");
+	
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # 2.21 simplify code
 # 2.23 finish
 # 2.24 finish
+# 2.25 finish
+
 
