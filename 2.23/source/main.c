9fa83167f09599c23ddfe3fd97da495a7544ddea
min4604 <mina4604sh@gmail.com>
2021-10-16 14:04:44 +0800

    2.23 finish

--- 2.23/source/main.c
+++ 2.23/source/main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-		   
+int num[3];
 
 void main(void)
 {
+	printf("Please write three number :");
+	scanf_s("%d %d %d", &num[0], &num[1], &num[2]);
 	
-	
+	for (int i = 3; i > 0; i--)
+	{
+		for (int b = 0; b < i; b++)
+		{
+			if (num[i] > num[b])
+			{
+				int save = num[i];
+				num[i] = num[b];
+				num[b] = save;
+			}
+		}
+	}
+	printf("largest number: %d\nsmallest number :%d\n",num[0],num[2]);
 	system("pause");
 	
 }
