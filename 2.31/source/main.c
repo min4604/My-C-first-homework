612382ee826adfb88b1697536b7d0e293185024a
min4604 <mina4604sh@gmail.com>
2021-10-17 09:25:39 +0800

    sat 2.31 prj

---
+++ 2.31/source/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void main(void)
+{
+	printf("    *\n");
+	printf("   ***\n");
+	printf("  *****\n");
+	printf(" *******\n");
+	printf("*********\n");
+	system("Pause");
+	printf("    *\n   ***\n  *****\n *******\n*********\n");
+	system("pause");
+	
+}
