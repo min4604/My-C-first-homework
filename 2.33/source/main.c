47f4a0b2b59db442b5a2b6466373c07b17d18b4f
min4604 <mina4604sh@gmail.com>
2021-10-17 11:13:04 +0800

    2.33 half

--- 2.33/source/main.c
+++ 2.33/source/main.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+float totalmiles, gasoline, gallon, parking, tollspar;
 
 void main(void)
 {
+	printf("Total miles driven per day:");
+	scanf_s("%f", &totalmiles);
+	printf("\nCost pergallon of gasoline:");
+	scanf_s("%f", &gasoline);
+	printf("\nAverage miles per gallon:");
+	scanf_s("%f", &gallon);
+	printf("\nparking feesper day:");
+	scanf_s("%f", &parking);
+	printf("\nTolls per day:");
+	scanf_s("%f", &tollspar);
+
+	printf("%.2f %.2f %.2f %.2f %.2f", totalmiles, gasoline, gallon, parking, tollspar);
 
 	system("pause");
 }
